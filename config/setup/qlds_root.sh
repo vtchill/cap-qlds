00711316ae3ccf3ae495aad6097dff0a2735c247
vtchill <manning.jesse@gmail.com>
2015-11-28 14:23:52 -0700

    update root setup script to add user to sudoers file

--- config/setup/qlds_root.sh
+++ config/setup/qlds_root.sh
@@ -12,15 +12,23 @@ if [ "$EUID" -ne 0 ]
 fi
 echo "Updating 'apt-get'..."
 apt-get update
+
 echo "Installing packages..."
 apt-get -y install python3 python-setuptools lib32gcc1 curl build-essential python-dev unzip wget lib32z1 lib32stdc++6 libc6
+
 echo "Adding user $QLDS_USER..."
 useradd -m $QLDS_USER
 usermod -a -G sudo $QLDS_USER
 chsh -s /bin/bash $QLDS_USER
+
 echo "Enter the password to use for $QLDS_USER account:"
 passwd $QLDS_USER
+
+echo "Adding user '$QLDS_USER' to sudoers file, and appending NOPASSWD..."
+echo "$QLDS_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
 echo "Installing Supervisor..."
 easy_install supervisor
+
 echo "All work done for 'root' user, please login to $QLDS_USER."
 exit
