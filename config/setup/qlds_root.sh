c2ab433a0c078371dca8e78e97f7ad392fbbefaf
vtchill <manning.jesse@gmail.com>
2015-11-28 14:12:21 -0700

    add setup scripts

---
+++ config/setup/qlds_root.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# qulds_root.sh - quake live dedicated server installation for root user.
+# intended to be run on a fresh VPS/Dedicated Server, this script must be run under the root user.
+# created by Thomas Jones on 29/09/15.
+# Edited by Jesse Manning 07/11/215.
+
+export QLDS_USER="steam"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run under user 'root'."
+  exit
+fi
+echo "Updating 'apt-get'..."
+apt-get update
+echo "Installing packages..."
+apt-get -y install python3 python-setuptools lib32gcc1 curl build-essential python-dev unzip wget lib32z1 lib32stdc++6 libc6
+echo "Adding user $QLDS_USER..."
+useradd -m $QLDS_USER
+usermod -a -G sudo $QLDS_USER
+chsh -s /bin/bash $QLDS_USER
+echo "Enter the password to use for $QLDS_USER account:"
+passwd $QLDS_USER
+echo "Installing Supervisor..."
+easy_install supervisor
+echo "All work done for 'root' user, please login to $QLDS_USER."
+exit
---
+++ config/setup/qlds_steam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# quakeinstall-qlserver.sh - quake live dedicated server installation for qlserver user.
+# intended to be run on a fresh VPS/Dedicated Server, this script must be run under the qlserver user.
+# created by Thomas Jones on 29/09/15.
+# edited by Jesse Manning on 07/11/15.
+
+export QLDS_USER="steam"
+
+if [ "$(whoami)" -ne "$QLDS_USER" ]
+  then echo "Please run under user '$QLDS_USER'."
+  exit
+fi
+clear
+echo "Installing SteamCMD..."
+mkdir ~/steamcmd
+cd ~/steamcmd
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -xvzf steamcmd_linux.tar.gz
+rm steamcmd_linux.tar.gz
+clear
+echo "Installing Quake Live Dedicated Server..."
+./steamcmd.sh +login anonymous +force_install_dir $HOME/steamcmd/steamapps/common/qlds/ +app_update 349090 +quit
+echo "Done."
+exit
